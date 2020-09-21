815561b8c60235c02f416b847eb9b1333fff17ab
sibenjol <63102845+sibenjol@users.noreply.github.com>
2020-09-21 18:36:37 +0700

    Update 9.php

--- 9.php
+++ 9.php
@@ -1,6 +1,6 @@
+**This is bold text**
 
-<p><a style="text-decoration:none;" href="//www.html.am">Link with no underline...</a></p>
-<p style="text-decoration:underline;">Normal text with underline</p>
-<p style="text-decoration:line-through;">Text with a line through the middle</p>
-<p style="text-decoration:overline;">Text with an overline</p>
-<p style="text-decoration:blink;">Blinking text (doesn't work on all browsers)</p>
+
+
+
+
